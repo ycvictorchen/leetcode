49acffb39fe2b4a8f4c17e8441d0489e61af76c1
Victor Chen <ycvictorchen@gmail.com>
2015-06-18 11:13:24 -0700

    #141

---
+++ LinkedListCycle.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            if ((fast = fast.next.next) == (slow = slow.next)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
